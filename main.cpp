7c5d937d0ec105680c5700391801367c672b4e8b
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-12 15:26:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "libreria.h"
+using namespace std;
+int main() {
+    int d=0;
+    cout<<"inserire il numero dei numeri";
+    cin>>d;
+    float arr[d];
+    riempi(arr,d);
+    cout<<massimo(arr,d);
 
-int main(){
-  return 0;
+
+
+
+
 }
