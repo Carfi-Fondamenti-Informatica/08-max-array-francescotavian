0fd2b96d07c829d23cc50ce985a45beb8849f609
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-12 15:28:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -7,14 +7,12 @@ void riempi(float arr[], int d)
     for (int i=0;i<d;i++){
         cin>>arr[i];
     }
-
 }
 float massimo(float arr[], int d) {
     float max=0;
     for (int i=0;i<d; i++){
         if (i==0){
             max=arr[i];
-
         }
         else if (arr[i]>max){
             max =arr[i];
