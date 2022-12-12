1f21f1cba7ca2cb082c9b225879342f07b51f882
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-12 15:35:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,5 @@
 
 #include "libreria.h"
-#include <iostream>
-using namespace std;
-void riempi(float arr[], int d)
-{
-    for (int i=0;i<d;i++){
-        cin>>arr[i];
-    }
-}
 float massimo(float arr[], int d) {
     float max=0;
     for (int i=0;i<d; i++){
